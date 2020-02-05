f7b6a724681c5c3f80dbcd85b20a3a556f27347c
mgelsthorpe <mgelsthorpe@hotmail.co.uk>
2020-02-05 21:42:20 +0000

    no correction needed just starting location

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -4,12 +4,13 @@ import pandas
 data = pandas.read_csv("volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+name = list(data["NAME"])
 
-maps = folium.Map(location=[52.988932, -1.311954], zoom_start=6, tiles= "stamen terrain")
+maps = folium.Map(location=[36.1699, -115.1398], zoom_start=5, tiles= "stamen terrain")
 fg=folium.FeatureGroup(name="my map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup= "hi i am a marker!", icon=folium.Icon(colour="green")))
+for lt, ln, n in zip(lat, lon, name):
+    fg.add_child(folium.Marker(location=[lt, ln], popup= "hi i am a marker!", icon=folium.Icon(colour="green"), tooltip=n))
 
 maps.add_child(fg)
 
